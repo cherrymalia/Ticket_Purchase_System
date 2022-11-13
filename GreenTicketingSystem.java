2face6f84210e69cfb057f4daa3adc0f81b1daf6
cherrymalia <35239061+cherrymalia@users.noreply.github.com>
2022-11-13 13:01:39 -0500

    updated generateTicket and main

--- GreenTicketingSystem.java
+++ GreenTicketingSystem.java
@@ -1,13 +1,22 @@
 
+import java.util.Scanner;
+
 public class GreenTicketingSystem {
     public static void main(String[] args) {
-
-        System.out.println("\nGreen Company Ticket Purchase System");
-        System.out.println("====================================");
-        //Scanner input = new Scanner(System.in);
+        Scanner repeat = new Scanner(System.in);
         Flight flight = new Flight();
-        flight.mainMenu();
 
+        System.out.println("\nWelcome to Green Company's Ticketing System!");
+        System.out.println("===============================================");
+
+        do {
+            //validate input
+            flight.mainMenu();
+            System.out.println("===============================================");
+            System.out.println("Would you like to return the main menu? (y/n)");
+        } while (repeat.nextLine().equals("y"));
+
+        repeat.close();
         System.out.println("Thank you for using the Green Company Ticket Purchase System");
 
     }
